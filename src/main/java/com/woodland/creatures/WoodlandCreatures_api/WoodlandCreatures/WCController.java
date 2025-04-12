7dbbfc2874ad48ae46c66402eaf45e60686f0f07
Carlos Taylor <cataylor5@uncg.edu>
2025-04-11 23:05:31 -0400

    saving work so far

--- src/main/java/com/woodland/creatures/WoodlandCreatures_api/WoodlandCreatures/WCController.java
+++ src/main/java/com/woodland/creatures/WoodlandCreatures_api/WoodlandCreatures/WCController.java
@@ -3,11 +3,12 @@ package com.woodland.creatures.WoodlandCreatures_api.WoodlandCreatures;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController // Returns JSON, not views
+@Controller // returns name of the webpage now
 @RequestMapping("/woodlandcreatures")
 public class WCController {
 
